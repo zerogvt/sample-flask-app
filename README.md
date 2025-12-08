4e161b1f14e2a838630ac530de9ac3ee06f59378
vasilis <gkoumplias@gmail.com>
2025-12-08 12:01:41 +0200

    upd

---
+++ README.md
@@ -0,0 +1,2 @@
+Reuse the local  daemon from Minikube with 
+`eval $(minikube docker-env)`
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from random import randrange
+from time import sleep
 app = Flask(__name__)
 
 from logging.config import dictConfig
@@ -28,7 +30,27 @@ dictConfig({
 def hello_world():
    #print("Hello, World! endpoint was reached")
    app.logger.info("Hello, World! endpoint was reached")
-   return "<h1>Hello, World!</h1>"
+   return "Hello, World!"
+
+
+@app.route("/server_error")
+def server_error():
+   app.logger.info("Server Error endpoint was reached")
+   return "server error", 500 
+
+
+@app.route("/client_error")
+def client_error():
+   app.logger.info("Client Error endpoint was reached")
+   return "client error", 400 
+
+@app.route("/latency")
+def latency():
+   sleep_time = randrange(1,10)
+   app.logger.info("latency endpoint was reached")
+   sleep(sleep_time)
+   return "client error", 400 
+
 if __name__ == "__main__":
    app.run(debug=True)
 
